3b592d5f2be09a8bda5b0ef3eef3b7bb9d535623
Rizqi Wijaya <rizwijaya@github.com>
2023-04-16 11:47:53 +0700

    fixing error handling in create and update activity.

--- modules/v1/activities/domain/input.go
+++ modules/v1/activities/domain/input.go
@@ -2,9 +2,9 @@ package domain
 
 type InsertActivity struct {
 	Title string `json:"title" binding:"required"`
-	Email string `json:"email" binding:"omitempty,email"`
+	Email string `json:"email" binding:"omitempty"`
 }
 type UpdateActivity struct {
-	Title string `json:"title" binding:"omitempty"`
-	Email string `json:"email" binding:"omitempty,email"`
+	Title string `json:"title" binding:"required"`
+	Email string `json:"email" binding:"omitempty"`
 }
--- modules/v1/activities/interfaces/controllers/activity_controller.go
+++ modules/v1/activities/interfaces/controllers/activity_controller.go
@@ -1,13 +1,13 @@
 package controllers
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"todolist-backend/modules/v1/activities/domain"
 	api "todolist-backend/pkg/api_response"
 	"todolist-backend/pkg/http_error"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -41,19 +41,27 @@ func (uc *ActivityController) GetActivityByID(c *fiber.Ctx) error {
 }
 
 func (uc *ActivityController) CreateActivity(c *fiber.Ctx) error {
-	var activityRequest domain.InsertActivity
-	if err := c.BodyParser(&activityRequest); err != nil {
+	var (
+		InsertActivity domain.InsertActivity
+		Validator      = validator.New()
+	)
+	//Parse and validate request body
+	if err := c.BodyParser(&InsertActivity); err != nil {
 		log.Println(err)
 		resp := api.NewErrorResponse("Bad Request", err.Error())
 		return c.Status(http.StatusBadRequest).JSON(resp)
 	}
-	//Check title is empty
-	if activityRequest.Title == "" {
-		resp := api.NewErrorResponse("Bad Request", "title cannot be null")
-		return c.Status(http.StatusBadRequest).JSON(resp)
+	//Validate request body input
+	err := Validator.Struct(InsertActivity)
+	if err != nil {
+		for _, v := range err.(validator.ValidationErrors) {
+			log.Println(err)
+			resp := api.NewErrorResponse("Bad Request", http_error.FormValidationError(v))
+			return c.Status(http.StatusBadRequest).JSON(resp)
+		}
 	}
 
-	activity, err := uc.activityUsecase.CreateActivity(activityRequest)
+	activity, err := uc.activityUsecase.CreateActivity(InsertActivity)
 	if err != nil {
 		log.Println(err)
 		resp := api.NewErrorResponse("Internal Server Error", "Internal Server Error")
@@ -73,21 +81,28 @@ func (uc *ActivityController) CreateActivity(c *fiber.Ctx) error {
 }
 
 func (uc *ActivityController) UpdateActivity(c *fiber.Ctx) error {
-	id := c.Params("id")
-	var UpdateActivity domain.UpdateActivity
+	var (
+		id             = c.Params("id")
+		UpdateActivity domain.UpdateActivity
+		Validator      = validator.New()
+	)
+	//Parse and validate request body
 	if err := c.BodyParser(&UpdateActivity); err != nil {
 		log.Println(err)
 		resp := api.NewErrorResponse("Bad Request", err.Error())
 		return c.Status(http.StatusBadRequest).JSON(resp)
 	}
-	//Check title is empty
-	if UpdateActivity.Title == "" {
-		resp := api.NewErrorResponse("Bad Request", "title cannot be null")
-		return c.Status(http.StatusBadRequest).JSON(resp)
+	//Validate request body input
+	err := Validator.Struct(UpdateActivity)
+	if err != nil {
+		for _, v := range err.(validator.ValidationErrors) {
+			log.Println(err)
+			resp := api.NewErrorResponse("Bad Request", http_error.FormValidationError(v))
+			return c.Status(http.StatusBadRequest).JSON(resp)
+		}
 	}
 
 	activity, err := uc.activityUsecase.UpdateActivity(id, UpdateActivity)
-	fmt.Println(activity)
 	if err != nil {
 		log.Println(err)
 		if http_error.IsSame(err, http_error.ErrRecordNotfound) {
